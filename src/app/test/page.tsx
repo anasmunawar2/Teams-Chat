a530e35937ce1cdaa6644238e45950105df3d06f
anasmunawar2 <anasmunawar2@gmail.com>
2024-10-03 23:37:37 +0500

    github auth added

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,16 +1,7 @@
 "use client";
 
-import { useQuery } from "convex/react";
-import React from "react";
-import { api } from "../../../convex/_generated/api";
-
 const Page = () => {
-  const tasks = useQuery(api.tasks.get);
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
-    </main>
-  );
+  return <h1>test</h1>;
 };
 
 export default Page;
