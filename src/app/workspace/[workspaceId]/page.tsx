17e9239c7f9f6b30cd4b34514b3a87771e29ce07
anasmunawar2 <anasmunawar2@gmail.com>
2024-10-11 01:13:56 +0500

    sidebar created

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,12 +1,5 @@
-"use client";
-
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-
 const WorkSpaceIdPage = () => {
-  const workspaceId = useWorkspaceId();
-  const { data } = useGetWorkspace({ id: workspaceId });
-  return <div>Data: {JSON.stringify(data)}</div>;
+  return <div>workspace id page</div>;
 };
 
 export default WorkSpaceIdPage;
