5664bc1f0c43b52b8d2eebb60e9bcd2a7773b1b1
anasmunawar2 <anasmunawar2@gmail.com>
2024-12-18 17:14:49 +0500

    workspace Id Page

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,4 +1,24 @@
+"use client";
+
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
+import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useRouter } from "next/router";
+
 const WorkSpaceIdPage = () => {
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
+  const [open, setOpen] = useCreateChannelModal();
+
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
+    id: workspaceId,
+  });
+
+  const { data: channels, isLoading: channelsLoading } = useGetChannels({
+    workspaceId,
+  });
+
   return <div>workspace id page</div>;
 };
 
