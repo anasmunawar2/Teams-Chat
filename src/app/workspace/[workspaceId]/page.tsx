5cede5f2524fc38a40ff6906fb64222fd7c36e15
anasmunawar2 <anasmunawar2@gmail.com>
2024-12-19 19:07:40 +0500

    workspace-loading

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -4,7 +4,9 @@ import { useGetChannels } from "@/features/channels/api/use-get-channels";
 import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { Loader, TriangleAlert } from "lucide-react";
 import { useRouter } from "next/router";
+import { useEffect, useMemo } from "react";
 
 const WorkSpaceIdPage = () => {
   const router = useRouter();
@@ -19,7 +21,46 @@ const WorkSpaceIdPage = () => {
     workspaceId,
   });
 
-  return <div>workspace id page</div>;
+  const channelId = useMemo(() => channels?.[0]?._id, [channels]);
+
+  useEffect(() => {
+    if (workspaceLoading || channelsLoading || !workspace) return;
+    if (channelId) {
+      router.push(`/workspace/${workspaceId}/channel/${channelId}`);
+    } else if (!open) {
+      setOpen(true);
+    }
+  }, [
+    channelId,
+    workspaceLoading,
+    channelsLoading,
+    workspace,
+    open,
+    setOpen,
+    router,
+    workspaceId,
+  ]);
+
+  if (workspaceLoading || channelsLoading) {
+    return (
+      <div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
+        <Loader className="size-6 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
+
+  if (!workspace) {
+    return (
+      <div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
+        <TriangleAlert className="size-6 animate-spin text-muted-foreground" />
+        <span className="text-sm text-muted-foreground">
+          Workspace not found
+        </span>
+      </div>
+    );
+  }
+
+  return null;
 };
 
 export default WorkSpaceIdPage;
