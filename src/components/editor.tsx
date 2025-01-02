897d4597194877258e6c3825f446b16793966745
anasmunawar2 <anasmunawar2@gmail.com>
2025-01-03 04:04:37 +0500

    making editor reuseable

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef } from "react";
+import React, { MutableRefObject, useEffect, useRef } from "react";
+import { Delta, Op } from "quill/core";
 import { PiTextAa } from "react-icons/pi";
 import { MdSend } from "react-icons/md";
 import Quill, { QuillOptions } from "quill";
@@ -7,7 +8,29 @@ import { Button } from "./ui/button";
 import { ImageIcon, Smile } from "lucide-react";
 import { Hint } from "./hint";
 
-const Editor = () => {
+type EditorValue = {
+  image: File | null;
+  body: string;
+};
+
+interface EditorProps {
+  onSubmit: ({ image, body }: EditorValue) => void;
+  onCancel?: () => void;
+  placeholder?: string;
+  defaultValue?: Delta | Op[];
+  disabled?: boolean;
+  innerRef?: MutableRefObject<Quill | null>;
+  variant?: "create" | "update";
+}
+const Editor = ({
+  onCancel,
+  onSubmit,
+  placeholder = "Write something...",
+  defaultValue = [],
+  disabled = false,
+  innerRef,
+  variant = "create",
+}: EditorProps) => {
   const containerRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     if (!containerRef.current) return;
@@ -58,25 +81,50 @@ const Editor = () => {
             </Button>
           </Hint>
 
-          <Hint label="Image">
+          {variant === "create" && (
+            <Hint label="Image">
+              <Button
+                disabled={false}
+                size="iconSm"
+                variant="ghost"
+                onClick={() => {}}
+              >
+                <ImageIcon className="size-4" />
+              </Button>
+            </Hint>
+          )}
+          {variant === "update" && (
+            <div className="ml-auto flex items-center gap-x-2">
+              <Button
+                disabled={false}
+                variant="outline"
+                size="sm"
+                onClick={() => {}}
+              >
+                Cancel
+              </Button>
+
+              <Button
+                disabled={false}
+                size="sm"
+                onClick={() => {}}
+                className="bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
+              >
+                Save
+              </Button>
+            </div>
+          )}
+
+          {variant === "create" && (
             <Button
-              disabled={false}
+              className="ml-auto bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
               size="iconSm"
-              variant="ghost"
+              disabled={false}
               onClick={() => {}}
             >
-              <ImageIcon className="size-4" />
+              <MdSend className="size-4" />
             </Button>
-          </Hint>
-
-          <Button
-            className="ml-auto bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
-            size="iconSm"
-            disabled={false}
-            onClick={() => {}}
-          >
-            <MdSend className="size-4" />
-          </Button>
+          )}
         </div>
       </div>
 
