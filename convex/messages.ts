ac20856d95b4def66c270d3a614a719e09444ffd
anasmunawar2 <anasmunawar2@gmail.com>
2025-01-16 17:25:36 +0500

    convex messages

---
+++ convex/messages.ts
@@ -0,0 +1,10 @@
+import { mutation } from "./_generated/server";
+
+export const create = mutation({
+  args: {},
+  handler: async (ctx) => {
+    // Implement the logic to create a workspace
+    // ...
+    // Return the created workspace
+  },
+});
