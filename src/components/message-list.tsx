1652845a612c229419a97706bd27d2ac1c681c12
anasmunawar2 <anasmunawar2@gmail.com>
2025-02-09 04:05:13 +0500

    message date

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -1,4 +1,5 @@
 import { GetMessagesReturnType } from "@/features/messages/api/use-get-messages";
+import { format, isToday, isYesterday } from "date-fns";
 
 interface MessageListProps {
   memberName?: string;
@@ -12,6 +13,13 @@ interface MessageListProps {
   canLoadMore: () => boolean;
 }
 
+const formatDateLabel = (dateStr: string) => {
+  const date = new Date(dateStr);
+  if (isToday(date)) return "Today";
+  if (isYesterday(date)) return "Yesterday";
+  return format(date, "EEEE, MMMM d");
+};
+
 const MessageList = ({
   memberName,
   channelName,
@@ -22,9 +30,30 @@ const MessageList = ({
   isLoadingMore,
   canLoadMore,
 }: MessageListProps) => {
+  const groupedMessages = data?.reduce(
+    (groups, message) => {
+      const date = new Date(message._creationTime);
+      const dateKey = format(date, "yyyy-MM-dd");
+      if (!groups[dateKey]) {
+        groups[dateKey] = [];
+      }
+      groups[dateKey].unshift(message);
+      return groups;
+    },
+    {} as Record<string, typeof data>
+  );
+
   return (
     <div className="flex-1 flex flex-col-reverse pb-4 overflow-y-auto messages-scrollbar">
-      {data?.map((message) => <div>{JSON.stringify(message)}</div>)}
+      {Object.entries(groupedMessages || {}).map(([dateKey, messages]) => (
+        <div key={dateKey}>
+          <div className="text-center my-2 relative">
+            <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300" />
+
+            <span>{formatDateLabel(dateKey)}</span>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
