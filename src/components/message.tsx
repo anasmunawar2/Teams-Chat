c1b2acbf3c644e9a5fc93879c804ce7d5c43285b
anasmunawar2 <anasmunawar2@gmail.com>
2025-02-10 16:44:19 +0500

    date-time formatter

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Doc, Id } from "../../convex/_generated/dataModel";
 import dynamic from "next/dynamic";
+import { format, isToday, isYesterday } from "date-fns";
+import { Hint } from "./hint";
 
 const Renderer = dynamic(() => import("@/components/renderer"), { ssr: false });
 
@@ -32,6 +34,11 @@ interface MessageProps {
   threadImage?: string;
   threadTimeStamp?: number;
 }
+
+const formatFullTime = (date: Date) => {
+  return `${isToday(date) ? "Today" : isYesterday(date) ? "Yesterday" : format(date, "MMM d, yyyy")} at ${format(date, "h:mm:ss a")}`;
+};
+
 const Message = ({
   id,
   memberId,
@@ -52,6 +59,13 @@ const Message = ({
 }: MessageProps) => {
   return (
     <div className="flex flex-col gap-2 p-1.5 px-5 hover:bg-gray-100/60 group relative">
+      <div className="flex items-start gap-2">
+        <Hint label={formatFullTime(new Date(createdAt))}>
+          <button className="text-xs text-muted-foreground opacity-0 group-hover:opacity-100 w-[40px] leading-[22px] text-center hover:underline">
+            {format(new Date(createdAt), "hh:mm")}
+          </button>
+        </Hint>
+      </div>
       <Renderer value={body} />
     </div>
   );
